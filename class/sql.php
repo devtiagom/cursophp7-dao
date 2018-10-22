b9b09a0f059ddf940d49821d1375903177eaaa4e
Tiago Machado <tiago.o.machado@gmail.com>
2018-10-22 20:57:41 -0300

    criamos a classe Usuario

--- class/sql.php
+++ class/sql.php
@@ -13,13 +13,13 @@
         }
 
         /***************************************************************************************************/
-        private function setParam($statment, $key, $value) {
-            $statment->bindParam($key, $value);
+        private function setParam($statement, $key, $value) {
+            $statement->bindParam($key, $value);
         }
 
-        private function setParams($statment, $parameters = array()) {
+        private function setParams($statement, $parameters = array()) {
             foreach ($parameters as $key => $value) {
-                $this->setParam($key, $value);
+                $this->setParam($statement, $key, $value);
             }
         }
 
